ef17ab7de5bf2209d37545a6afec381bd93f1026
Sahand <sahandkarimkhani@gmail.com>
2023-09-20 16:54:20 -0400

    init react & react-router-dom

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <h1>my HomePage</h1>
+}
+
+export default HomePage;
---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,5 @@
+function ProductPage() {
+    return <h1>my ProductPage</h1>
+}
+
+export default ProductPage;
