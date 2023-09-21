bafc24067b6ba93ff5e90993d50863c5bf49c801
nima <nimamahini81@gmail.com>
2023-09-21 15:03:18 +0330

    make changes

---
+++ backend/release.config.js
@@ -0,0 +1,20 @@
+const config = {
+    branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/git",
+            {
+                assests: ["dist/*.js", "dist/*.js.map"],
+                message:
+                    "chore(release): ${nextRelease.version} [skip ci] \n\n${nextRelease.notes}",
+            },
+        ],
+        "@semantic-release/github",
+    ],
+};
+
+module.exports = {
+    config,
+};
