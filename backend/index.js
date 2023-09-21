4de8558046c8bc6fc0d1eb86a23a536a885c9987
nima <nimamahini81@gmail.com>
2023-09-21 17:30:47 +0330

    handel online DB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
 const Application = require("./app/server")
-new Application(5000, "mongodb://localhost:27017/Store_project")
-//nima
-// mmd
+new Application(5000, "mongodb://root:XGqfGniSRJO8sdDeibGYmIuu@aberama.iran.liara.ir:33605/my-app?authSource=admin")
+// new Application(5000, "mongodb://localhost:27017/Store_project")
